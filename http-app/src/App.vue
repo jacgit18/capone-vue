2df66ac16cff682a9420bf0cb426204f19ca46fd
jacgit18 <joshuaxcarpentier@gmail.com>
2024-06-06 13:49:15 -0400

    added working code

--- http-app/src/App.vue
+++ http-app/src/App.vue
@@ -1,17 +1,30 @@
-
 <template>
-<div>
-  Test 
-</div>
+  <user-survey @survey-submit="storeSurvey"></user-survey>
 </template>
 
 <script>
-
+import UserSurvey from "./components/UserSurvey.vue";
 export default {
-
+  components: {
+    UserSurvey,
+  },
+  data() {
+    return {
+      savedSurveyResults: [],
+    };
+  },
+  methods: {
+    storeSurvey(surveyData) {
+      const surveyResult = {
+        name: surveyData.userName,
+        rating: surveyData.rating,
+        id: new Date().toISOString(),
+      };
+      this.savedSurveyResults.push(surveyResult);
+      console.log(surveyResult);
+    },
+  },
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- http-app/src/main.js
+++ http-app/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import BaseButton from './shared/BaseButton.vue'
+import BaseCard from './shared/BaseCard.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.component('base-card', BaseCard)
+app.component('base-button', BaseButton)
+
+app.mount('#app')
