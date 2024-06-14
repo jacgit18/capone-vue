1ce446ff8436aadb4ce00d3fde965060470c7902
jacgit18 <joshuaxcarpentier@gmail.com>
2024-06-14 00:30:16 -0400

    edit

--- main-project/src/views/MealList.vue
+++ main-project/src/views/MealList.vue
@@ -1,10 +1,44 @@
 <template>
-    <div class="bg-gray-100 h-100">
-   
+  <div class="flex flex-col p-8">
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded-border-2 border-red-200 w-fill"
+      placeholder="Search for todays menu"
+      @change="searchMeals"
+    />
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+      <div v-for="meal of meals" :key="meal.idMeal" class="bg-blue shadow rounded">
+        <img :src="meal.strMealThumb" />
+        <h3 class="p-3 font-semibold">{{ meal.strMeal }}</h3>
+        <div class="p-3">
+          <a :href="meal.strYoutube" target="_blank"
+          class="px-3 py-2 rounded border-2 text-white border-orange-600 bg-orange-500 hover:bg-orange-600 transition-colors"
+          >YouTube</a>
+          <router-link to="">View</router-link>
+        </div>
+      </div>
     </div>
-  </template>
-  
-  <script></script>
-  
-  <style></style>
-  
+  </div>
+</template>
+
+<script setup>
+import { computed } from "@vue/reactivity";
+import { ref } from "vue";
+import store from "../store";
+
+const keyword = ref("");
+
+const meals = computed(() => store.state.searchedMeals);
+
+function searchMeals() {
+  if (keyword.value) {
+    store.dispatch("searchMeals", keyword.value);
+  } else {
+    store.commit("setSearchedMeals", []);
+  }
+}
+</script>
+
+<style scoped></style>
