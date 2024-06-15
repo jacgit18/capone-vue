8fdc6a95c4d3791868ed698590c2aa5bd33450f6
jacgit18 <joshuaxcarpentier@gmail.com>
2024-06-15 01:59:29 -0400

    edit

--- main-project/src/store/actions.js
+++ main-project/src/store/actions.js
@@ -1,7 +1,7 @@
-import {axiosClient} from "../axiosClient"
+import { axiosClient } from "../axiosClient";
 
 
-export const actions = {
+export const searchMeals = {
     searchMeals: ({ commit }, keyword) => {
       axiosClient.get(`/search.php?s=${keyword}`)
         .then(({ data }) => {
@@ -15,3 +15,17 @@ export const actions = {
     },
   };
   
+
+  export const searchMealsByLetter = {
+    searchMeals: ({ commit }, keyword) => {
+      axiosClient.get(`/search.php?s=${keyword}`)
+        .then(({ data }) => {
+          // Ensure to log or process data correctly
+          console.log(data);
+          commit('setSearchedMeals', data.meals); // Adjust 'data.searchMeals' if needed
+        })
+        .catch(error => {
+          console.error('Error fetching meals:', error);
+        });
+    },
+  };
